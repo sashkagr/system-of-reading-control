4f9e6c4bcf5c9541ddb721a4489b04b136edb1ab
n31 <nikkovlev@gmail.com>
2023-09-21 20:48:00 +0200

    users table and basic structure

--- create_database.sql
+++ create_database.sql
@@ -1,1 +1,11 @@
+DROP DATABASE IF EXISTS system_of_reading_control;
+CREATE DATABASE system_of_reading_control;
+USE system_of_reading_control;
 
+CREATE TABLE users
+(
+    id INT AUTO_INCREMENT NOT NULL,
+    login VARCHAR(255),
+    password VARCHAR(255),
+    PRIMARY KEY (id)
+);
